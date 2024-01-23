674167d59b4814f3ddec78a184547019f4ebbac4
Golam Mourshid <golam.mourshid@tigerit.com>
2024-01-23 15:26:49 +0600

    Implemented scenario outline for taking input from .feature file

--- src/test/java/stepDefinitions/LoginStepDefinitions.java
+++ src/test/java/stepDefinitions/LoginStepDefinitions.java
@@ -10,15 +10,16 @@ import setup.BrowserSetup;
 public class LoginStepDefinitions extends BrowserSetup {
     LoginPage loginPage;
     DashboardPage dashboardPage;
+
     @Given("user visits the websites")
     public void user_visits_the_websites() {
         driver.get("https://www.saucedemo.com/");
     }
 
-    @When("user enters username and password")
-    public void user_enters_username_and_password() {
+    @When("user enters username {string} and password {string}")
+    public void userEntersUsernameAndPassword(String username, String password) {
         loginPage = new LoginPage(driver);
-        loginPage.doLogin("standard_user", "secret_sauce");
+        loginPage.doLogin(username, password);
     }
 
     @When("user clicks on login button")
--- src/test/resources/login.feature
+++ src/test/resources/login.feature
@@ -1,6 +1,12 @@
 Feature: Login feature
-  Scenario: login with valid credentials
+
+  Scenario Outline: login with valid credentials
     Given user visits the websites
-    When user enters username and password
+    When user enters username "<username>" and password "<password>"
     And user clicks on login button
     Then user can browse the website
+    Examples:
+      | username                | password     |
+      | standard_user           | secret_sauce |
+      | problem_user            | secret_sauce |
+      | performance_glitch_user | secret_sauce |
